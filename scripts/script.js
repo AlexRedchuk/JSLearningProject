2c60ba2e541f982184ed5ddffc359a8862e6c0b5
Alexander <sanya.redchuk@gmail.comr>
2020-10-05 18:55:34 +0300

    Practice 015

--- scripts/script.js
+++ scripts/script.js
@@ -9,10 +9,57 @@ let personalMovieDB = {
     privat: false
 };
 
-let lastMovie = prompt("Один из последних просмотреных фильмов?");
-let mark = +prompt("На сколько вы его оцените");
+// for (let i = 0; i<2; i++) {
+//     let lastMovie = prompt("Один из последних просмотреных фильмов?");
+    
+//     let mark = +prompt("На сколько вы его оцените");
+
+//     if ( (lastMovie.length === 0 || lastMovie.length > 50) || (mark.length === 0 || mark.length > 50)) {
+//         i--;
+//     }
+//     else {
+//         personalMovieDB.movies[lastMovie] = mark;
+//     }
+// }
+
+let i = 0;
+// while(i < 2) {
+//     let lastMovie = prompt("Один из последних просмотреных фильмов?");
+    
+//     let mark = +prompt("На сколько вы его оцените");
+
+//     if ( (lastMovie.length === 0 || lastMovie.length > 50) || (mark.length === 0 || mark.length > 50)) {
+//         i--;
+//     }
+//     else {
+//         personalMovieDB.movies[lastMovie] = mark;
+//     }
+//     i++;
+// }
+
+do {
+    let lastMovie = prompt("Один из последних просмотреных фильмов?"); 
+    let mark = +prompt("На сколько вы его оцените");
+    if ( (lastMovie.length === 0 || lastMovie.length > 50) || (mark.length === 0 || mark.length > 50)) {
+                i--;
+    }
+    else {
+        personalMovieDB.movies[lastMovie] = mark;
+    }
+    i++;
+}
+while(i<2);
+
+if(personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >=10 && personalMovieDB.count <=30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
 
 
-personalMovieDB.movies[lastMovie] = mark;
 
 console.log(personalMovieDB);
